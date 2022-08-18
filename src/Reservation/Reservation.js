e1a9dae62c23fb2d779f3070135e1db03bbb801a
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2022-08-18 18:34:42 +0300

    update: add style

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -5,29 +5,32 @@ import { LocalizationProvider } from "@mui/x-date-pickers-pro";
 import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 import { AdapterDateFns } from "@mui/x-date-pickers-pro/AdapterDateFns";
 import Box from "@mui/material/Box";
+import style from "../Reservation/Reservation.module.scss";
 
 const Reservation = () => {
   const [value, setValue] = React.useState([null, null]);
 
   return (
-    <LocalizationProvider
-      dateAdapter={AdapterDateFns}
-      localeText={{ start: "Check-in", end: "Check-out" }}
-    >
-      <DateRangePicker
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(startProps, endProps) => (
-          <React.Fragment>
-            <TextField {...startProps} />
-            <Box sx={{ mx: 2 }}> to </Box>
-            <TextField {...endProps} />
-          </React.Fragment>
-        )}
-      />
-    </LocalizationProvider>
+    <div className={style.Reservation}>
+      <LocalizationProvider
+        dateAdapter={AdapterDateFns}
+        localeText={{ start: "Check-in", end: "Check-out" }}
+      >
+        <DateRangePicker
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(startProps, endProps) => (
+            <React.Fragment>
+              <TextField {...startProps} />
+              <Box sx={{ mx: 2 }}> to </Box>
+              <TextField {...endProps} />
+            </React.Fragment>
+          )}
+        />
+      </LocalizationProvider>
+    </div>
   );
 };
 
